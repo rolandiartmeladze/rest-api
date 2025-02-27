996310ff652c9019b83cc8b40da1c0771df26225
rolandiartmeladze <rartmeladze@gmail.com>
2025-02-28 03:12:02 +0400

    opdate/ code

--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,16 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
-import { hostname } from 'os';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
+  app.enableCors();
   app.useStaticAssets(join(__dirname, '..', 'public'));
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('pug');
 
-  await app.listen(process.env.PORT || 3000, '0.0.0.0');
+
+  await app.listen(3000);
 }
 bootstrap();
